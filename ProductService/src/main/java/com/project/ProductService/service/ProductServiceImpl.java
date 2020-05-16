3a78184fb12f849b01e28603402dd6de3dd61396
5AAbatch <64475807+5AAbatch@users.noreply.github.com>
2020-05-16 17:25:31 +0530

    Committed by Deepthi Inguva

--- ProductService/src/main/java/com/project/ProductService/service/ProductServiceImpl.java
+++ ProductService/src/main/java/com/project/ProductService/service/ProductServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -41,13 +42,21 @@ public class ProductServiceImpl implements ProductService {
 		
 			RestTemplate restTemplate = new RestTemplate();
 		
-			 ConversionPojo conversionPojo = restTemplate.getForObject("http://localhost:8083/ConversionService/con/1", ConversionPojo.class);
+			 ResponseEntity<ConversionPojo[]> conversionPojo = restTemplate.getForEntity("http://localhost:8083/ConversionService/all", ConversionPojo[].class);
 		
-			int a = conversionPojo.getValue();
-			
+			 ConversionPojo[] conPojo = conversionPojo.getBody();
+			 int[] result = new int[3];
+			 
+			 
+			for(int i=0;i<conPojo.length ; i++)  {
+			int	a = conPojo[i].getValue();
 			int price = productEntity.getPrice();
 			
-			int result = price / a;
+			 result[i] = price / a;
+			}
+			
+			
+			
 		
 			ProductPojo productPojo = new ProductPojo(productEntity.getId(),
 													  productEntity.getName(),
@@ -56,7 +65,9 @@ public class ProductServiceImpl implements ProductService {
 													  productEntity.getStock(),
 													  productEntity.getDescription(),
 													  productEntity.getRemarks(),
-													  result);
+													  result[0],
+													  result[1],
+													  result[2]);
 			
 			allProductPojo.add(productPojo);
 		}
